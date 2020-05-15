c4867e017bdee7452984b4db4a2331cd06841325
richardwochenende <richard@wochenende-events.de>
2020-05-15 18:55:19 +0200

    still testing
    Signed-off-by: richardwochenende <richard@wochenende-events.de>

--- MMM-GrandTotal.js
+++ MMM-GrandTotal.js
@@ -5,13 +5,6 @@ Module.register("MMM-GrandTotal",{
 		text: "FirstModuleInput"
 	},
 
-	// Override dom generator.
-	getDom: function() {
-		var wrapper = document.createElement("div");
-		wrapper.innerHTML = this.config.text;
-		return wrapper;
-	},
-	
 	loaded: function(callback) {
 		this.finishLoading();
 		Log.log(this.name + ' is loaded!');
@@ -30,27 +23,5 @@ Module.register("MMM-GrandTotal",{
 		this.file('anotherfile.js'), // this file will be loaded straight from the module folder.
 		'https://code.jquery.com/jquery-2.2.3.min.js',  // this file will be loaded from the jquery servers.
 		]
-	},
-
-	getStyles: function() {
-	return [
-		'script.css', // will try to load it from the vendor folder, otherwise it will load is from the module folder.
-		'font-awesome.css', // this file is available in the vendor folder, so it doesn't need to be available in the module folder.
-		this.file('anotherfile.css'), // this file will be loaded straight from the module folder.
-		'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css',  // this file will be loaded from the bootstrapcdn servers.
-		]
-	},
-
-	getTranslations: function() {
-		return {
-			en: "translations/en.json",
-			de: "translations/de.json"
-			}
-	},
-
-	getDom: function() {
-		var wrapper = document.createElement("div");
-		wrapper.innerHTML = 'Hello world!';
-		return wrapper;
 	}
 });
