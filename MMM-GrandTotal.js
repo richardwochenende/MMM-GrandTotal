f01f289e723cbe2551d6990aa477530d306c9e91
richardwochenende <richard@wochenende-events.de>
2020-05-15 19:02:44 +0200

    Testing
    Signed-off-by: richardwochenende <richard@wochenende-events.de>

--- MMM-GrandTotal.js
+++ MMM-GrandTotal.js
@@ -1,27 +1,19 @@
 Module.register("MMM-GrandTotal",{
-
 	// Default module config.
 	defaults: {
-		text: "FirstModuleInput"
+		text: "Hello World!"
+	},
+
+	// Override dom generator.
+	getDom: function() {
+		var wrapper = document.createElement("div");
+		wrapper.innerHTML = this.config.text;
+		return wrapper;
 	},
 
 	loaded: function(callback) {
 		this.finishLoading();
 		Log.log(this.name + ' is loaded!');
 		callback();
-	},
-
-	start: function() {
-		this.mySpecialProperty = "So much wow!";
-		Log.log(this.name + ' is started!');
-	},
-
-	getScripts: function() {
-	return [
-		'script.js', // will try to load it from the vendor folder, otherwise it will load is from the module folder.
-		'moment.js', // this file is available in the vendor folder, so it doesn't need to be available in the module folder.
-		this.file('anotherfile.js'), // this file will be loaded straight from the module folder.
-		'https://code.jquery.com/jquery-2.2.3.min.js',  // this file will be loaded from the jquery servers.
-		]
 	}
 });
